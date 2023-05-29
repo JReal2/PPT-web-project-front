ffc4fc9280234c1bbe6b718469fad0b101d7c179
강지원 <kangji149@naver.com>
2023-05-30 00:49:27 +0900

    회원가입 서버연동 완료

---
+++ Login&SignUp/app.js
@@ -0,0 +1,51 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+const signUpForm = document.querySelector('.sign-up-form');
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+// onclick 함수 정의
+function signUp(form)
+{
+    const name = signUpForm.querySelector('input[type="text"]').value;
+    const email = signUpForm.querySelector('input[type="email"]').value;
+    const password = signUpForm.querySelector('input[type="password"]').value;
+    const company = signUpForm.querySelector('input[placeholder="Company"]').value;
+    const career = signUpForm.querySelector('input[placeholder="Career"]').value;
+    const skill = signUpForm.querySelector('input[placeholder="Skill"]').value;
+    const interestSkill = signUpForm.querySelector('input[placeholder="Interest skill"]').value;
+   
+
+    data = JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+        company: company,
+        career: career,
+        skill: skill,
+        interestSkill: interestSkill
+    });
+    console.log(data)
+    var xhr = new XMLHttpRequest(); // XMLHttpRequest 객체 생성
+    xhr.open("POST", 'http://localhost:8080'+'/members/new', true); //요청을 보낼 방식, 주소, 비동기 여부 설정
+    xhr.setRequestHeader('Content-Type', 'application/json'); //요청 해더에 컨텐츠 타입 Json으로 사전 정의
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === xhr.DONE) {
+            if (xhr.status === 200) { //연결 성공시
+                alert("success");
+                location.href='index.html';                                                             
+            }
+            else {
+                alert("Existed email or under 6 letters password.")
+            }
+        }
+    }
+    xhr.send(data); //Json형식의 data를 포함하여 요청 전송
+}
